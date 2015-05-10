e7a2d9fdd112f0af3c2de1fd32db33090a81e56d
Damian Finol <damian.finol@gmail.com>
2015-05-09 21:08:43 -0700

    0.0.11 Support for Intents

--- bin/alexa_rubyengine.rb
+++ bin/alexa_rubyengine.rb
@@ -15,11 +15,33 @@ post '/' do
   # Check that it's a valid Alexa request
   request_json = JSON.parse(request.body.read.to_s)
   # Creates a new Request object with the request parameter.
-  request = AlexaRubykit::Request.new(request_json)
+  request = AlexaRubykit.build_request(request_json)
+
+  # We also need a response object.
+  response = AlexaRubykit::Response.new
+
+  # We can manipulate the request object.
+  #
+  #p "#{request.to_s}"
+  #p "#{request.request_id}"
 
   # Response
-  response = AlexaRubykit::Response.new
-  response.add_speech('Ruby is running ready!')
-  response.add_hash_card( { :title => 'Ruby Run', :subtitle => 'Ruby Running Ready!' } )
+  # If it's a launch request
+  if (request.type == 'LAUNCH_REQUEST')
+    # Process your Launch Request
+    # Call your methods for your application here that process your Launch Request.
+    response.add_speech('Ruby running ready!')
+    response.add_hash_card( { :title => 'Ruby Run', :subtitle => 'Ruby Running Ready!' } )
+  end
+
+  if (request.type == 'INTENT_REQUEST')
+    # Process your Intent Request
+    p "#{request.slots}"
+    p "#{request.name}"
+    response.add_speech("I received an intent named #{request.name}?")
+    response.add_hash_card( { :title => 'Ruby Intent', :subtitle => "Intent #{request.name}" } )
+  end
+
+  # Return response
   response.build_response
 end
